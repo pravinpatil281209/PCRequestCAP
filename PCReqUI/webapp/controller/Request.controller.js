e8c349890bbbfe6f73c9203740261e58de93f725
pravinpatil281209 <pravin.p@lntinfotech.com>
2020-09-17 13:49:36 +0000

    added comments

--- PCReqUI/webapp/controller/Request.controller.js
+++ PCReqUI/webapp/controller/Request.controller.js
@@ -6,7 +6,7 @@ sap.ui.define([
 
 	return Controller.extend("com.lti.pc.PCReqUI.controller.Request", {
 		onInit: function () {
-
+		//added comment
 		},		
 		onSaveChanges: function (oEvent) {
 		    var that = this;
